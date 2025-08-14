61b05118c7a4e42e5222159de87d1ea98ed42fa1
ismaelp96 <ismael6991@live.com>
2025-08-14 14:01:05 -0300

    feat: gerando dados fictícios

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -2,9 +2,8 @@ import { beforeEach, describe, expect, it } from 'vitest';
 
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
 import { GetQuestionBySlugUseCase } from './get-question-by-slug';
-import { Question } from '../../enterprise/entities/question';
+import { makeQuestion } from 'test/factories/make-question';
 import { Slug } from '../../enterprise/entities/value-objects/slug';
-import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: GetQuestionBySlugUseCase;
@@ -15,12 +14,8 @@ describe('Get question by slug', () => {
 		sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
 	});
 	it('should be able to get a question by slug', async () => {
-		const newQuestion = Question.create({
-			title: 'Example question',
-			slug: Slug.create('example-question'),
-			authorId: new UniqueEntityID(),
-			content: 'example content',
-		});
+		const newQuestion = makeQuestion({ slug: Slug.create('example-question') });
+
 		await inMemoryQuestionsRepository.create(newQuestion);
 		const { question } = await sut.execute({ slug: 'example-question' });
 
