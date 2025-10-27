3f5f05f37323ab26c67ef26c9e0ad03bcb0886cf
Ismaelp96 <ismael6991@live.com>
2025-10-27 14:41:26 -0300

    caso de uso: comentar na pergunta

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+import { makeQuestion } from 'test/factories/make-question';
+import { InMemoryQuestionCommentRepository } from 'test/repositories/in-memory-question-comment-repository';
+import { CommentOnQuestionUseCase } from './comment-on-question';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionCommentRepository: InMemoryQuestionCommentRepository;
+let sut: CommentOnQuestionUseCase;
+
+describe('Commment on Question', () => {
+	beforeEach(() => {
+		inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+		inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository();
+		sut = new CommentOnQuestionUseCase(
+			inMemoryQuestionsRepository,
+			inMemoryQuestionCommentRepository,
+		);
+	});
+
+	it('should be able to cooment on question', async () => {
+		const question = makeQuestion();
+
+		await inMemoryQuestionsRepository.create(question);
+
+		await sut.execute({
+			questionId: question.id.toString(),
+			authorId: question.authorId.toString(),
+			content: 'Comentário teste',
+		});
+
+		expect(inMemoryQuestionCommentRepository.items[0].content).toEqual(
+			'Comentário teste',
+		);
+	});
+});
